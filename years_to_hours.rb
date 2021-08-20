30065761d897077ee0ca7dccbd3d42a7c0803d60
Adriana <56941883+adrianagithub@users.noreply.github.com>
2021-08-19 19:12:56 -0700

    Hi Shy again you are doing a great job. you already submit your second assignment.  I just have one comment regarding to be more specific, other than that you did an amazing job.
    puts "Enter a number of decades"
    decades = gets.chomp 
    decades = decades.to_i 
    minutes = decades * 10 * 365 * 24 * 60
    #In this I will add the put this way to be more specific like this puts "That's #{minutes} minutes in #{decades}."
    puts "That's #{minutes} minutes."
    
    
    puts "Enter your age"
    age = gets.chomp 
    age = age.to_i 
    age = age * 365 * 24 * 60 * 60
    #In this I will add the put this way to be more specific -> puts "That's #{seconds} seconds in your age #{age}."
    puts "That's #{age} seconds."

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -8,10 +8,13 @@ puts "Enter a number of decades"
 decades = gets.chomp 
 decades = decades.to_i 
 minutes = decades * 10 * 365 * 24 * 60
+#In this I will add the put this way to be more specific like this puts "That's #{minutes} minutes in #{decades}."
 puts "That's #{minutes} minutes."
+
 
 puts "Enter your age"
 age = gets.chomp 
 age = age.to_i 
 age = age * 365 * 24 * 60 * 60
+#In this I will add the put this way to be more specific -> puts "That's #{seconds} seconds in your age #{age}."
 puts "That's #{age} seconds."
